91f06ca6064b046bcf14a569c3c27f0a239b3359
MoiseenkoEvgenia <sokolova-2206@mail.ru>
2022-10-29 17:41:48 +0300

    add Zadacha 27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int sum = 0;
+while (a > 0)
+{
+    sum = sum + a % 10;
+    a = a / 10;
+}
+Console.WriteLine(sum);
