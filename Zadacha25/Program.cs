d5894ca63cb62b973e14fc4829365bc6720b68aa
MoiseenkoEvgenia <sokolova-2206@mail.ru>
2022-10-29 21:37:56 +0300

    add Zadacha 25 исправление

--- Zadacha25/Program.cs
+++ Zadacha25/Program.cs
@@ -3,11 +3,21 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.WriteLine("Введите число: ");
+int ChislovStepeni(int a, int b)
+{
+    int rezult = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        rezult = rezult * a;
+    }
+    return rezult;
+}
+
+Console.WriteLine("Введите число A: ");
 int a = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("Введите число: ");
-int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число B: ");
+int b = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine($"{Math.Pow(a, n)}");
-
+int chislovStepeni = ChislovStepeni(a, b);
+Console.WriteLine($"Ответ: {chislovStepeni}");
