18de9cf5fa68700df588fbfccefd3c3cb1f4d2b2
MoiseenkoEvgenia <sokolova-2206@mail.ru>
2022-10-29 18:45:11 +0300

    add Zadacha 29

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+Console.WriteLine("Введите количество элементов массива:\t");
+int count = int.Parse(Console.ReadLine());
+int[] array = new int[count];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите элемент массива под индексом {i}:\t ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Вывод массива: ");
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+     Console.Write($"{array[i]}, ");
+}
+Console.Write("]");
+Console.Read();
